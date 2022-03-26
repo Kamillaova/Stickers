9982a986efde1aaae1b026e1e91ad995150913c1
Kamillaova <54859825+Kamillaova@users.noreply.github.com>
2022-03-26 20:18:46 +0300

    trash

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,39 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+        google()
+    }
+
+    plugins {
+        val agpVersion = "7.1.2"
+        val safeArgsVersion = "2.4.1"
+        val kotlinVersion = "1.6.10"
+
+        kotlin("jvm") version kotlinVersion
+        kotlin("android") version kotlinVersion
+        kotlin("plugin.serialization") version kotlinVersion
+
+        id("com.android.library") version agpVersion
+        id("com.android.application") version agpVersion
+        id("androidx.navigation.safeargs.kotlin") version safeArgsVersion
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+}
+
 rootProject.name = "Stickers"
-include("android")
-include("api")
-include("desktop")
-include("shared")
-include("web")
+
+arrayOf(
+    "android",
+    "api",
+    "desktop",
+    "shared"
+).forEach { include(":$it") }
---
+++ shared/build.gradle.kts
@@ -0,0 +1,5 @@
+plugins {
+    kotlin("jvm")
+}
+
+// TODO: Use kotlin("multiplatform")
